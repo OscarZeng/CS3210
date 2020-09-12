047e420a61eeb268013263538f7853886db5c626
OscarZeng <zenghaonus@outlook.com>
2020-09-12 16:41:05 +0800

    Update LCS-parallel.c

--- assignment1_code/LCS-parallel.c
+++ assignment1_code/LCS-parallel.c
@@ -1,13 +1,39 @@
 #include <pthread.h>       
 #include <stdio.h>
 #include <stdlib.h>
-#define THREADS 8
+#define NUM_THREADS 1
 
 //For global varibles
 char *seq1;
 char *seq2;
 int *mat[2];
 int M, N;
+
+void* work(void* threadid) // function to run in parallel
+{
+    long tid;
+    int old = 0;
+    int new = 1;
+    for (int i = 1; i <= M; i++) {
+        for (int j = 1; j <= N; j++) {
+            if (seq1[i - 1] == seq2[j - 1]) {
+                mat[new][j] = mat[old][j - 1] + 1;
+            } else {
+                int left = mat[new][j - 1];
+                int down = mat[old][j];
+                mat[new][j] = left > down ? left : down;
+            }
+            //printf("%d   ", mat[i][j]);
+        }
+        // Swap the previous and current row
+        old = old ^ new;
+        new = new ^ old;
+        old = old ^ new;
+        //printf("\n");
+    }
+    printf("thread #%ld implementing\n", tid);
+    pthread_exit(NULL); // terminating thread
+}
 
 int main(int argc, char *argv[]) {
     // Accepts filepaths to two input DNA sequences as command-line arguments
@@ -41,28 +67,27 @@ int main(int argc, char *argv[]) {
     mat[1] = malloc((N + 1) * sizeof(int));
     mat[1][0] = 0;
 
-    int old = 0;
-    int new = 1;
-    for (int i = 1; i <= M; i++) {
-        for (int j = 1; j <= N; j++) {
-            if (seq1[i - 1] == seq2[j - 1]) {
-                mat[new][j] = mat[old][j - 1] + 1;
-            } else {
-                int left = mat[new][j - 1];
-                int down = mat[old][j];
-                mat[new][j] = left > down ? left : down;
-            }
-            //printf("%d   ", mat[i][j]);
+    pthread_t threads[NUM_THREADS];
+    int rc;
+    long t;
+    for (t = 0; t < NUM_THREADS; t++) {
+        printf("main thead: creating thread %ld\n", t);
+
+        // pthread_create spawns a new thread and return 0 on success
+        rc = pthread_create(&threads[t], NULL, work, (void*)t);
+        if (rc) {
+            printf("Return code from pthread_create() is %d\n", rc);
+            exit(-1);
         }
-        // Swap the previous and current row
-        old = old ^ new;
-        new = new ^ old;
-        old = old ^ new;
-        //printf("\n");
     }
 
+/*    
+    
+
+*/
     printf("%d", mat[old][N]);
 
+    pthread_exit(NULL);
     free(seq1);
     free(seq2);
     free(mat[0]);
