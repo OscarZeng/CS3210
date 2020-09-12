8fb21253311beee89837455472f7f2f8d34b5f63
OscarZeng <zenghaonus@outlook.com>
2020-09-12 15:33:41 +0800

    Update LCS-parallel.c

--- assignment1_code/LCS-parallel.c
+++ assignment1_code/LCS-parallel.c
@@ -1,6 +1,53 @@
+#include <pthread.h>
+#include <queue>          
 #include <stdio.h>
 #include <stdlib.h>
+#define PRODUCERS 8
+#define CONSUMERS 8
 
+//For global varibles
+char *seq1;
+char *seq2;
+int *mat[2];
+int M, N;
+/*
+//Generates tuple to input into the key
+void* producer(void* threadid)
+{
+    long pid;
+    pid = (long) threadid;
+    //When using the producer_buffer, it enters the critical session.
+    while(1){
+        if (ind < 10){
+            //Generate a random number between 1 to 10
+            int inputNum = 1+(rand()%10);
+            producer_buffer[ind] = inputNum;
+            ind = ind + 1;
+            printf("producer thread #%d added a number %d to the producer_buffer now with size: %d\n",pid, inputNum, ind);
+        }
+        sleep(1);
+    }
+
+}
+
+void* consumer(void* threadid)
+{
+    long cid;
+    cid = (long) threadid;
+    while(1){
+        if (ind > 0){
+            //Get the number and update the sum
+            ind = ind - 1;
+            int frontNumber = producer_buffer[ind];
+            producer_buffer[ind] = 0;
+            consumer_sum = consumer_sum + frontNumber;
+            printf("consumer thread #%d added a number %d from the producer_buffer now with size: %d, consumer_sum is :%d\n",cid, frontNumber, ind, consumer_sum);
+        }
+        sleep(1);
+    }
+
+}
+*/
 int main(int argc, char *argv[]) {
     // Accepts filepaths to two input DNA sequences as command-line arguments
     if (argc != 3) {
@@ -16,7 +63,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    int M, N;
+
     fscanf(file1, "%d", &M);
     fscanf(file2, "%d", &N);
 
@@ -27,9 +74,15 @@ int main(int argc, char *argv[]) {
     fscanf(file2, "%s", seq2);
     fclose(file1);
     fclose(file2);
-
+/*
+    //Before this line is file reading part
+    pthread_t producer_threads[PRODUCERS];
+    pthread_t consumer_threads[CONSUMERS];
+    long producer_threadid[PRODUCERS];
+    long consumer_threadid[CONSUMERS];
+*/
     // Initialise two rows of the matrix with (M + 1) rows x (N + 1) columns
-    int *mat[2];
+    
     mat[0] = calloc(N + 1, sizeof(int));
     mat[1] = malloc((N + 1) * sizeof(int));
     mat[1][0] = 0;
@@ -45,11 +98,13 @@ int main(int argc, char *argv[]) {
                 int down = mat[old][j];
                 mat[new][j] = left > down ? left : down;
             }
+            printf("%d   ", mat[i][j]);
         }
         // Swap the previous and current row
         old = old ^ new;
         new = new ^ old;
         old = old ^ new;
+        printf("\n");
     }
 
     printf("%d", mat[old][N]);
